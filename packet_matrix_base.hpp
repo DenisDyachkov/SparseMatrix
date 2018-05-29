6e01ccc000ea3e508425dca90b129b351c719aed
DenisDyachkov <denisrafdyachkov@yandex.ru>
2018-05-29 15:02:54 +0700

    Базовый интерфейс разреженных матриц
    Добавлен базовый интерфейс разреженных матриц

---
+++ packet_matrix_base.hpp
@@ -0,0 +1,13 @@
+#ifndef SPARSEMATRIX_PACKET_MATRIX_BASE_HPP
+#define SPARSEMATRIX_PACKET_MATRIX_BASE_HPP
+#include <cinttypes>
+#include <vector>
+
+class IPacketMatrixBase {
+public:
+    virtual size_t getNZ() const = 0;
+    virtual size_t getN() const = 0;
+    virtual uint64_t getMatrixSize() const = 0;
+};
+
+#endif //SPARSEMATRIX_PACKET_MATRIX_BASE_HPP
