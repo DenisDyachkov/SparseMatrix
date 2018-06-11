89af73c46c4b255731f89fc775de7840e3b6c79a
Denis Dyachkov <denisrafdyachkov@yandex.ru>
2018-06-11 14:04:25 +0700

    Fix data path

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     mcoord_t matrix;
-    matrix.loadFromMatrixMarketFile("test data\\matrix_430_430.mtx");
+    matrix.loadFromMatrixMarketFile("test data/matrix_430_430.mtx");
     CEvaluation::storageEfficiency(matrix);
     CEvaluation::storageEfficiency(mdiag_t(matrix));
     CEvaluation::storageEfficiency(msdiag_t32(matrix));
